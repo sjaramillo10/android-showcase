231446732944ec70148dccc27ec72c5fd6dcc509
Severiano Jaramillo <severiano.jaramillo@gmail.com>
2020-05-26 11:03:19 -0500

    Add deep link instructions.

--- feature_album/src/main/java/com/igorwojda/showcase/feature/album/presentation/albumdetail/AlbumDetailActivity.kt
+++ feature_album/src/main/java/com/igorwojda/showcase/feature/album/presentation/albumdetail/AlbumDetailActivity.kt
@@ -13,6 +13,10 @@ class AlbumDetailActivity : BaseActivity() {
         super.onResume()
         underConstructionAnimation.playAnimation()
 
+        // Execute one of the following commands to open this activity from a deep link
+        // adb shell am start -W -a android.intent.action.VIEW -d "showcase://detail"
+        // OR
+        // adb shell am start -W -a android.intent.action.VIEW -d "showcase://detail" com.igorwojda.showcase
         intent.data?.let {
             Snackbar.make(findViewById(android.R.id.content),
                 "Came from a deep link!", Snackbar.LENGTH_SHORT).show()
